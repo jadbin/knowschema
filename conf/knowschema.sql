5e0691ce3ef175957331768aec2f989f14e56999
从鑫 <cxcongxin@gmail.com>
2020-03-23 16:20:18 +0800

    add sql structure

--- conf/knowschema.sql
+++ conf/knowschema.sql
@@ -1,105 +1,126 @@
 /*
  Navicat Premium Data Transfer
 
- Source Server         : localhost
+ Source Server         : mysql
  Source Server Type    : MySQL
- Source Server Version : 50721
- Source Host           : localhost
- Source Database       : knowschema
+ Source Server Version : 50729
+ Source Host           : localhost:3306
+ Source Schema         : knowschema
 
  Target Server Type    : MySQL
- Target Server Version : 50721
- File Encoding         : utf-8
+ Target Server Version : 50729
+ File Encoding         : 65001
 
- Date: 03/18/2020 15:37:36 PM
+ Date: 23/03/2020 16:19:50
 */
 
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
 
 -- ----------------------------
---  Table structure for `clause`
+-- Table structure for clause
 -- ----------------------------
 DROP TABLE IF EXISTS `clause`;
-CREATE TABLE `clause` (
+CREATE TABLE `clause`  (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `uri` varchar(255) NOT NULL,
-  `content` varchar(255) DEFAULT NULL,
-  `level` int(255) DEFAULT NULL,
-  `field_id` int(11) DEFAULT NULL,
+  `uri` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `content` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `level` int(255) NULL DEFAULT NULL,
+  `field_id` int(11) NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE,
-  UNIQUE KEY `uri` (`uri`),
-  KEY `clause_field` (`field_id`) USING BTREE,
-  CONSTRAINT `clause_field` FOREIGN KEY (`field_id`) REFERENCES `field` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=934 DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
+  UNIQUE INDEX `uri`(`uri`) USING BTREE,
+  INDEX `clause_field`(`field_id`) USING BTREE,
+  CONSTRAINT `clause_field` FOREIGN KEY (`field_id`) REFERENCES `field` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE = InnoDB AUTO_INCREMENT = 1080 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
---  Table structure for `clause_entity_type_mapping`
+-- Table structure for clause_entity_type_mapping
 -- ----------------------------
 DROP TABLE IF EXISTS `clause_entity_type_mapping`;
-CREATE TABLE `clause_entity_type_mapping` (
+CREATE TABLE `clause_entity_type_mapping`  (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `concept_id` int(11) NOT NULL,
   `object_id` int(11) NOT NULL,
   `clause_id` int(11) NOT NULL,
   PRIMARY KEY (`id`) USING BTREE,
-  KEY `clause` (`clause_id`) USING BTREE,
-  KEY `object` (`object_id`),
-  KEY `concept` (`concept_id`) USING BTREE,
-  CONSTRAINT `clause` FOREIGN KEY (`clause_id`) REFERENCES `clause` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1358 DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
+  INDEX `clause`(`clause_id`) USING BTREE,
+  INDEX `object`(`object_id`) USING BTREE,
+  INDEX `concept`(`concept_id`) USING BTREE,
+  CONSTRAINT `clause` FOREIGN KEY (`clause_id`) REFERENCES `clause` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE = InnoDB AUTO_INCREMENT = 1591 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
---  Table structure for `entity_type`
+-- Table structure for entity_type
 -- ----------------------------
 DROP TABLE IF EXISTS `entity_type`;
-CREATE TABLE `entity_type` (
+CREATE TABLE `entity_type`  (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `uri` varchar(255) NOT NULL,
-  `display_name` varchar(255) NOT NULL,
-  `description` varchar(255) DEFAULT NULL,
-  `icon` varchar(255) DEFAULT NULL,
-  `father_id` int(11) DEFAULT '0',
-  `has_child` tinyint(255) DEFAULT '0',
-  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `uri` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `display_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `description` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `icon` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `father_id` int(11) NULL DEFAULT 0,
+  `has_child` tinyint(255) NULL DEFAULT 0,
+  `created_at` timestamp(0) NULL DEFAULT CURRENT_TIMESTAMP(0),
+  `updated_at` timestamp(0) NULL DEFAULT CURRENT_TIMESTAMP(0) ON UPDATE CURRENT_TIMESTAMP(0),
   PRIMARY KEY (`id`) USING BTREE,
-  KEY `uri` (`uri`) USING BTREE,
-  KEY `father_id` (`father_id`) USING BTREE,
-  KEY `has_child` (`has_child`) USING BTREE
-) ENGINE=InnoDB AUTO_INCREMENT=3983 DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC;
+  INDEX `uri`(`uri`) USING BTREE,
+  INDEX `father_id`(`father_id`) USING BTREE,
+  INDEX `has_child`(`has_child`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 4659 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
---  Table structure for `field`
+-- Table structure for field
 -- ----------------------------
 DROP TABLE IF EXISTS `field`;
-CREATE TABLE `field` (
+CREATE TABLE `field`  (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `uri` varchar(255) DEFAULT NULL,
-  `title` varchar(255) NOT NULL,
+  `uri` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `title` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
---  Table structure for `property_type`
+-- Table structure for operation_record
+-- ----------------------------
+DROP TABLE IF EXISTS `operation_record`;
+CREATE TABLE `operation_record`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `operator` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `operation_type` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `entity_type_id` int(11) NULL DEFAULT NULL,
+  `entity_type_uri` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `property_type_id` int(11) NULL DEFAULT NULL,
+  `property_type_uri` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `operated_field` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `original_value` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `new_value` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `operated_at` timestamp(0) NULL DEFAULT CURRENT_TIMESTAMP(0) ON UPDATE CURRENT_TIMESTAMP(0),
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `property_type_id`(`property_type_id`) USING BTREE,
+  INDEX `entity_type_id`(`entity_type_id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 151 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for property_type
 -- ----------------------------
 DROP TABLE IF EXISTS `property_type`;
-CREATE TABLE `property_type` (
+CREATE TABLE `property_type`  (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `uri` varchar(255) NOT NULL,
-  `display_name` varchar(255) NOT NULL,
-  `description` varchar(255) DEFAULT NULL,
-  `icon` varchar(255) DEFAULT NULL,
-  `field_type` varchar(255) DEFAULT NULL,
-  `is_entity` tinyint(4) DEFAULT '0',
+  `uri` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `display_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `description` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `icon` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `field_type` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `is_entity` tinyint(4) NULL DEFAULT 0,
   `entity_type_id` int(11) NOT NULL,
-  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `created_at` timestamp(0) NULL DEFAULT CURRENT_TIMESTAMP(0),
+  `updated_at` timestamp(0) NULL DEFAULT CURRENT_TIMESTAMP(0) ON UPDATE CURRENT_TIMESTAMP(0),
   PRIMARY KEY (`id`) USING BTREE,
-  KEY `uri` (`uri`) USING BTREE,
-  KEY `entity_type_id` (`entity_type_id`) USING BTREE,
-  KEY `is_entity` (`is_entity`) USING BTREE,
-  CONSTRAINT `property_type_ibfk_1` FOREIGN KEY (`entity_type_id`) REFERENCES `entity_type` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=905 DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC;
+  INDEX `uri`(`uri`) USING BTREE,
+  INDEX `entity_type_id`(`entity_type_id`) USING BTREE,
+  INDEX `is_entity`(`is_entity`) USING BTREE,
+  CONSTRAINT `property_type_ibfk_1` FOREIGN KEY (`entity_type_id`) REFERENCES `entity_type` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE = InnoDB AUTO_INCREMENT = 1426 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
 SET FOREIGN_KEY_CHECKS = 1;
