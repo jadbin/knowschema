3ad62f03334dab710b9e51af23a8dadbaf9ee9fa
jadbin <jadbin.com@hotmail.com>
2020-04-08 10:19:52 +0800

    upgrade guniflask

--- conf/knowschema.py
+++ conf/knowschema.py
@@ -1,8 +1,11 @@
 # coding=utf-8
 
-# jwt = True
-# Secret for JWT
-JWT_SECRET = '0pDcIczkJ7ZvLqL353_sGm5Rwf_KNQSFjiXWT3rV9Tg'
-
 # Database URI, example: mysql://username:password@server/db?charset=utf8mb4
 SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:123456@localhost/knowschema?charset=utf8mb4'
+
+
+# guniflask configuration
+guniflask = dict(
+    cors=True,
+    authorization_server='https://uaa.kdsec.org',
+)
--- knowschema/app.py
+++ knowschema/app.py
@@ -19,7 +19,12 @@ def init_app(app, settings):
     """
     This function is invoked before running app.
     """
-    db.init_app(app)
+    _init_sqlalchemy(app, settings)
 
-    # from knowschema.load_clause import load_clause
-    # load_clause(app)
+
+def _init_sqlalchemy(app, settings):
+    db.init_app(app)
+    do_wrap = settings.get_by_prefix('guniflask.wrap_sqlalchemy_model', True)
+    if do_wrap:
+        from guniflask.orm import wrap_sqlalchemy_model
+        wrap_sqlalchemy_model(db.Model)
