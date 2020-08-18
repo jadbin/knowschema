1ef66984563603072819b3c8d7dd8cd61cd00f3a
从鑫 <cxcongxin@gmail.com>
2020-08-18 10:41:24 +0800

    add stat

--- knowschema/controllers/main_page.py
+++ knowschema/controllers/main_page.py
@@ -39,14 +39,28 @@ class MainPageController:
             "total_entity_type_num": 0,
             "level_1_num": 0,
             "level_2_num": 0,
-            "level_3_num": 0
+            "level_3_num": 0,
+            "object_level_1_num": 0,
+            "object_level_2_num": 0,
+            "object_level_3_num": 0,
+            "concept_level_1_num": 0,
+            "concept_level_2_num": 0,
+            "concept_level_3_num": 0,
         }
         level_1_entity_type = EntityType.query.filter_by(father_id=0).all()
         common_user_entity_type_num['level_1_num'] = len(level_1_entity_type)
+        common_user_entity_type_num['object_level_1_num'] = len([i for i in level_1_entity_type if i.is_object == 1])
+        common_user_entity_type_num['concept_level_1_num'] = len([i for i in level_1_entity_type if i.is_object == 0])
+
         level_2_entity_type = [j for i in level_1_entity_type for j in EntityType.query.filter_by(father_id=i.id).all()]
         common_user_entity_type_num['level_2_num'] = len(level_2_entity_type)
+        common_user_entity_type_num['object_level_2_num'] = len([i for i in level_2_entity_type if i.is_object == 1])
+        common_user_entity_type_num['concept_level_2_num'] = len([i for i in level_2_entity_type if i.is_object == 0])
+
         level_3_entity_type = [j for i in level_2_entity_type for j in EntityType.query.filter_by(father_id=i.id).all()]
         common_user_entity_type_num['level_3_num'] = len(level_3_entity_type)
+        common_user_entity_type_num['object_level_3_num'] = len([i for i in level_3_entity_type if i.is_object == 1])
+        common_user_entity_type_num['concept_level_3_num'] = len([i for i in level_3_entity_type if i.is_object == 0])
 
         common_user_entity_type_num['total_entity_type_num'] = common_user_entity_type_num['level_1_num'] + common_user_entity_type_num['level_2_num'] + common_user_entity_type_num['level_3_num']
 
